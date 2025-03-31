88d7e4e55e12f1c0d06d9439d9e2b6b92a2ba5cb
Adya Pattanaik <apattan@purdue.edu>
2025-03-30 23:41:01 -0400

    Initial Commit

---
+++ PetAdoption/PetAdoptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAdoptionApp.swift
+//  PetAdoption
+//
+//  Created by Adya Shreya Pattanaik on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PetAdoptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetAdoptionTests/PetAdoptionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetAdoptionTests.swift
+//  PetAdoptionTests
+//
+//  Created by Adya Shreya Pattanaik on 3/30/25.
+//
+
+import Testing
+@testable import PetAdoption
+
+struct PetAdoptionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
